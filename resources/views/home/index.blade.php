798cbf6ac89cd7ad1532e2faa89d671a3b7bd583
JudithCanulC <90659921+JudithCanulC@users.noreply.github.com>
2022-12-19 09:02:03 -0500

    modified:   resources/views/home/index.blade.php
    Untracked files:
    	docker-compose.yml.txt

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,13 +3,14 @@
 @section('content')
 <div class="row">
 <div class="col-md-6 col-lg-4 mb-2">
-<img src="{{ asset('/img/game.png') }}" class="img-fluid rounded">
+<img src=https://primerbucket12.s3.amazonaws.com/img/game.png
+class="img-fluid rounded">
 </div>
 <div class="col-md-6 col-lg-4 mb-2">
-<img src="{{ asset('/img/safe.png') }}" class="img-fluid rounded">
+<img src=https://primerbucket12.s3.amazonaws.com/img/safe.png class="img-fluid rounded">
 </div>
 <div class="col-md-6 col-lg-4 mb-2">
-<img src="{{ asset('/img/submarine.png') }}" class="img-fluid rounded">
+<img src=https://primerbucket12.s3.amazonaws.com/img/submarine.png class="img-fluid rounded">
 </div>
 </div>
 @endsection
